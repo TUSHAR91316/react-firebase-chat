73ea158d324686f1396e61163570e11b7575ae93
TUSHAR91316 <tushar123913@gmail.com>
2024-06-03 19:46:41 +0530

    fix issue of emoji not showing in chatbar

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,6 +1,14 @@
+import EmojiPicker from "emoji-picker-react"
 import "./chat.css"
+import { useState } from "react"
 
 const Chat =()=>{
+    const[open,setOpen]=useState(false)
+    const[text,setText]=useState("")
+    const handleEmoji= e =>{
+        setText((prev)=> prev + e.emoji);
+        setOpen(false)
+    };
     return(
         <div className='chat'>
             <div className="top">
@@ -19,13 +27,16 @@ const Chat =()=>{
             <div className="center"></div>
             <div className="bottom">
                 <div className="icons">
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                    <img src="./img.png" alt="" />
+                    <img src="./camera.png" alt="" />
+                    <img src="./mic.png" alt="" />
                 </div>
-                <input type="text" placeholder="Type a message ..." />\
+                <input type="text" placeholder="Type a message ..." 
+                value={text}
+                onChange={(e) => setText(e.target.value)}/>
                 <div className="emoji">
-                    <img src="./emoji.png" alt="" />
+                    <img src="./emoji.png" alt=""onClick={()=>setOpen((prev) => !prev)} />
+                    <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
                 </div>
                 <button className="sendButton">Send</button>
             </div>
