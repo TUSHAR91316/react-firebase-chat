8ac0e862a390ddc4ebfb5e462f85f3a0aa4132c9
TUSHAR91316 <tushar123913@gmail.com>
2024-06-03 19:45:04 +0530

    fix issue regarding emoji addition of emoji sticker

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -17,7 +17,18 @@ const Chat =()=>{
                 </div>
             </div>
             <div className="center"></div>
-            <div className="bottom"></div>
+            <div className="bottom">
+                <div className="icons">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+                <input type="text" placeholder="Type a message ..." />\
+                <div className="emoji">
+                    <img src="./emoji.png" alt="" />
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
         </div>
     )
 }
