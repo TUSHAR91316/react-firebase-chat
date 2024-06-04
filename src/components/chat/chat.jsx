58371c6633bfed7c05f07b6dc51e54ccf110104e
TUSHAR91316 <tushar123913@gmail.com>
2024-06-04 17:57:31 +0530

    make chat component light work on details fix emoji animation.

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -24,7 +24,41 @@ const Chat =()=>{
                     <img src="./info.png" alt="" />
                 </div>
             </div>
-            <div className="center"></div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p></p>
+                        <span> </span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="text">
+                        <p></p>
+                        <span> </span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p></p>
+                        <span> </span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="text">
+                        <p></p>
+                        <span> </span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p></p>
+                        <span> </span>
+                    </div>
+                </div>
+            </div>
             <div className="bottom">
                 <div className="icons">
                     <img src="./img.png" alt="" />
@@ -35,8 +69,12 @@ const Chat =()=>{
                 value={text}
                 onChange={(e) => setText(e.target.value)}/>
                 <div className="emoji">
-                    <img src="./emoji.png" alt=""onClick={()=>setOpen((prev) => !prev)} />
-                    <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    <img src="./emoji.png" 
+                    alt=""
+                    onClick={()=>setOpen((prev) => !prev)} />
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>  
                 </div>
                 <button className="sendButton">Send</button>
             </div>
--- src/components/details/Detail.jsx
+++ src/components/details/Detail.jsx
@@ -2,7 +2,39 @@ import "./detail.css"
 
 const Detail =()=>{
     return(
-        <div className='detail'>Detail</div>
+        <div className='detail'>
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2></h2>
+                <p></p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy&help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Photo</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
